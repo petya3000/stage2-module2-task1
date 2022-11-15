142eca4e9e99896dee5fe1044fa4c95a85176ccb
Petya4000 <pt.bartosh54321@gmail.com>
2022-11-15 03:21:02 +0300

    first try

--- src/main/java/com/example/User.java
+++ src/main/java/com/example/User.java
@@ -6,8 +6,7 @@ public class User {
     private String firstName;
     private String lastName;
 
-    public User() {
-    }
+    public User() {}
 
     public User(String firstName, String lastName) {
         this.firstName = firstName;
--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -1,5 +1,31 @@
 package com.example.servlet;
 
-public class AddUserServlet {
-    //write your code here!
+import com.example.User;
+import com.example.Warehouse;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/add")
+public class AddUserServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        try{
+            req.getRequestDispatcher("/add.jsp").forward(req,res);
+        } catch (Exception ignored){}
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res){
+        try {
+            User user = new User(req.getParameter("firstName"), req.getParameter("secondName"));
+            Warehouse.getInstance().addUser(user);
+            req.setAttribute("add", user);
+            res.sendRedirect("/add");
+        } catch (Exception ignored){}
+    }
 }
